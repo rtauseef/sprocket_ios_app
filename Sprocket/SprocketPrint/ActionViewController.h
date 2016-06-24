d56670ab88f676cf1646a5fcc21988a5d9700857
Chrissy Harris <chrissy@hp.com>
2016-06-23 18:24:54 -0700

    [US4691] Blank action sheet!

---
+++ Sprocket/SprocketPrint/ActionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActionViewController.h
+//  SprocketPrint
+//
+//  Created by Susy Snowflake on 6/23/16.
+//  Copyright © 2016 HP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActionViewController : UIViewController
+
+@end
