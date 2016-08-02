637039958cdfeb03c89d4ac3b825343bc87919ea
hemathokachichu <hema.thokachichu@hp.com>
2016-08-02 15:41:04 -0700

    updated landing page step definition

--- Sprocket/features/step_definitions/landingscreen_steps.rb
+++ Sprocket/features/step_definitions/landingscreen_steps.rb
@@ -12,33 +12,36 @@ Then(/^I should see "(.*?)" logo$/) do |arg|
         if arg=="Camera Roll"
           check_element_exists(@current_page.cameraroll_logo)
         else
-        if arg=="Sprocket"
-        else
-          check_element_exists(@current_page.sprocket_logo)
+          if arg=="Sprocket"
+            check_element_exists(@current_page.sprocket_logo)
+          else
+            if arg=="Hamburger"
+              check_element_exists(@current_page.hamburger_logo)
+            end
+          end
         end
       end
     end
-end
   end
-end   
+end
 
 Then(/^I tap "(.*?)"$/) do |social_source|
   if element_exists("UIImageView id:'#{social_source}'")
-      touch ("UIImageView id:'#{social_source}'")
+    touch ("UIImageView id:'#{social_source}'")
   end
-    swipe_coach_marks_view
+  swipe_coach_marks_view
 end
 
-  And(/^I should see social source authentication text$/) do
-    sleep(MIN_TIMEOUT)
-    exp_auth_text="By authenticating with social sources you also agree to HP Terms of Service."
-    compare = (@current_page.social_source_auth_text == exp_auth_text) ? true : false
-    raise "Authentication text mismatch" unless compare==true
-  end
+And(/^I should see social source authentication text$/) do
+  sleep(MIN_TIMEOUT)
+  exp_auth_text="By authenticating with social sources you also agree to HP Terms of Service."
+  compare = (@current_page.social_source_auth_text == exp_auth_text) ? true : false
+  raise "Authentication text mismatch" unless compare==true
+end
 
-  When(/^I touch the Terms of Service link$/) do
-    @current_page.terms_of_service_link
-    
-    
-    sleep(WAIT_SCREENLOAD)
-  end
+When(/^I touch the Terms of Service link$/) do
+  @current_page.terms_of_service_link
+
+
+  sleep(WAIT_SCREENLOAD)
+end
