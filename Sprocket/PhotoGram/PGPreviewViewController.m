284cf38160ebf7adc6c68ebede911a3e6c3fcbbf
Chrissy Harris <chrissy@hp.com>
2016-08-09 13:32:27 -0700

    [US4887] Crop - rotate 3:2 crop selections

--- Sprocket/PhotoGram/PGPreviewViewController.m
+++ Sprocket/PhotoGram/PGPreviewViewController.m
@@ -74,6 +74,8 @@ static CGFloat const kPGPreviewViewControllerFlashTransitionDuration = 0.4F;
 
 - (void)viewWillAppear:(BOOL)animated
 {
+    static BOOL firstAppearance = YES;
+    
     [self.view layoutIfNeeded];
     
     [super viewWillAppear:animated];
@@ -90,7 +92,10 @@ static CGFloat const kPGPreviewViewControllerFlashTransitionDuration = 0.4F;
         } else {
             [self showCamera];
         }
-        
+    }
+    
+    if (firstAppearance) {
+        firstAppearance = NO;
         CGRect frame = self.imageContainer.frame;
         
         CGFloat aspectRatioWidth = [self paper].width;
@@ -202,7 +207,7 @@ static CGFloat const kPGPreviewViewControllerFlashTransitionDuration = 0.4F;
 
 - (void)showImgly
 {
-    IMGLYPhotoEditViewController *photoController = [[IMGLYPhotoEditViewController alloc] initWithPhoto:self.imageView.image configuration:[self imglyConfiguration]];
+    IMGLYPhotoEditViewController *photoController = [[IMGLYPhotoEditViewController alloc] initWithPhoto:[self.imageContainer screenshotImage] configuration:[self imglyConfiguration]];
     IMGLYToolStackController *toolController = [[IMGLYToolStackController alloc] initWithPhotoEditViewController:photoController configuration:[self imglyConfiguration]];
     toolController.delegate = self;
     
@@ -305,9 +310,12 @@ static CGFloat const kPGPreviewViewControllerFlashTransitionDuration = 0.4F;
             [textToolBuilder setTitle:@" "];
             
             [textToolBuilder setTextFieldConfigurationClosure:^(UITextField * _Nonnull textField) {
+                static NSInteger numTextFields = 0;
+                
                 [textField setKeyboardAppearance:UIKeyboardAppearanceDark];
                 [textField setTextAlignment:NSTextAlignmentCenter];
                 [textField setTintColor:[UIColor whiteColor]];
+                [textField setAccessibilityIdentifier:[NSString stringWithFormat:@"txtField%ld", (long)numTextFields]];
             }];
         }];
 
@@ -388,8 +396,8 @@ static CGFloat const kPGPreviewViewControllerFlashTransitionDuration = 0.4F;
 
 - (void)toolStackController:(IMGLYToolStackController * _Nonnull)toolStackController didFinishWithImage:(UIImage * _Nonnull)image
 {
-    NSLog(@"IMAGE:  %@", image);
-    self.imageView.image = image;
+    self.selectedPhoto = image;
+    self.imageView.image = self.selectedPhoto;
     [self dismissViewControllerAnimated:YES completion:nil];
     [self.imageView layoutIfNeeded];
 }
