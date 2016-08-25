0d57a7699cf70deb667d8c1c231c70955995c4d3
Chrissy Harris <chrissy@hp.com>
2016-08-25 12:50:17 -0700

    [US4778] Minor details

--- Sprocket/PhotoGram/PGAboutViewController.m
+++ Sprocket/PhotoGram/PGAboutViewController.m
@@ -12,6 +12,7 @@
 
 #import "PGAboutViewController.h"
 #import "UIFont+Style.h"
+#import "UIColor+Style.h"
 #import "UIViewController+Trackable.h"
 #import "PGLoggingSetttingsViewController.h"
 #import "PGAppAppearance.h"
@@ -23,6 +24,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *copyrightLabel;
 @property (weak, nonatomic) IBOutlet UILabel *companyLabel;
 @property (weak, nonatomic) IBOutlet UIButton *secretMenuButton;
+@property (weak, nonatomic) UIWebView *sprocketView;
 
 @end
 
@@ -51,7 +53,67 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (IBAction)secretMenuButtonTapped:(id)sender {
+- (IBAction)sprocketsButtonTapped:(id)sender {
+    static int requiredNumberOfTaps       = 3;
+    static NSTimeInterval maxSecondsToTap = 3;
+    static NSDate *tapStartTime           = nil;
+    static int numTaps                    = 0;
+    
+    NSTimeInterval secondsElapsed = -[tapStartTime timeIntervalSinceNow];
+    
+    if( numTaps >= requiredNumberOfTaps ) {
+        numTaps = 0;
+        
+        CGFloat yOrigin = 50;
+        if ([sender isKindOfClass:[UIButton class]]) {
+            yOrigin = ((UIButton *)sender).frame.origin.y - 10;
+        }
+        
+        UIImage *image = [UIImage imageNamed:@"Sprocket.gif"];
+        CGRect frame = CGRectMake((self.view.frame.size.width - image.size.width)/2, yOrigin,
+                                  image.size.width, image.size.height);
+        UIWebView *webview = [[UIWebView alloc]initWithFrame:frame];
+        NSURL *nsurl = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"Sprocket.gif" ofType:nil]];
+        NSURLRequest *nsrequest=[NSURLRequest requestWithURL:nsurl];
+        [webview loadRequest:nsrequest];
+        
+        UIButton *dismissButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        frame.origin.y += frame.size.height + 10;
+        frame.size.height = 44;
+        dismissButton.frame = frame;
+        [dismissButton.titleLabel setFont:self.versionLabel.font];
+        [dismissButton setTitleColor:[UIColor HPBlueButtonColor] forState:UIControlStateNormal];
+        [dismissButton setTitle:@"My viewing experience is complete" forState:UIControlStateNormal];
+        [dismissButton addTarget:self action:@selector(sprocketButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [self.view addSubview:webview];
+        [self.view addSubview:dismissButton];
+        self.sprocketView = webview;
+    } else if( nil == tapStartTime ||
+              maxSecondsToTap < secondsElapsed ) {
+        
+        tapStartTime = [NSDate date];
+        numTaps = 0;
+        
+    } else if ( maxSecondsToTap >= secondsElapsed ) {
+        
+        numTaps++;
+    }
+}
+
+- (IBAction)sprocketButtonTapped:(id)sender
+{
+    if (self.sprocketView) {
+        [self.sprocketView removeFromSuperview];
+    }
+    
+    if ([sender isKindOfClass:[UIButton class]]) {
+        [((UIButton *)sender) removeFromSuperview];
+    }
+}
+
+- (IBAction)secretMenuButtonTapped:(id)sender
+{
     static int requiredNumberOfTaps       = 10;
     static NSTimeInterval maxSecondsToTap = 5;
     static NSDate *tapStartTime           = nil;
