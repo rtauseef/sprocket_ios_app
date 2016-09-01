5363ad7b451aa0ef4cb8f97d903cfb0a987701e0
Ivan Gialorenço <ivan.gialorenco@arctouch.com>
2016-09-01 13:50:45 -0700

    Correct URL

--- Sprocket/PhotoGram/PGHelpAndHowToViewController.m
+++ Sprocket/PhotoGram/PGHelpAndHowToViewController.m
@@ -12,8 +12,14 @@
 
 #import "PGHelpAndHowToViewController.h"
 #import "UIViewController+Trackable.h"
+#import "PGWebViewerViewController.h"
 
 #import <Social/Social.h>
+#import <MP.h>
+
+NSString * const kPGHelpAndHowToViewUserURL = @"https://www.hpsprocket.com/sprocket-userguide.pdf";
+NSString * const kPGHelpAndHowToVisitWebsiteURL = @"http://support.hp.com/us-en/products/printers/";
+NSString * const kPGHelpAndHowToJoinForumSupportURL = @"http://h30434.www3.hp.com/t5/Printers/ct-p/InkJet";
 
 @interface PGHelpAndHowToViewController ()
 
@@ -31,8 +37,6 @@
     self.trackableScreenName = @"Help and How To Screen";
     
     self.twitterComposeViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
-    [self.twitterComposeViewController setInitialText:@"First post from my iPhone app"];
-    [self.twitterComposeViewController addURL:[NSURL URLWithString:@"http://www.appcoda.com"]];
     [self.twitterComposeViewController addImage:[UIImage imageNamed:@"test.jpg"]];
     
     __weak typeof(self) weakSelf = self;
@@ -45,10 +49,41 @@
 {
     if (indexPath.row == 1) {
         if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
+            NSString *tweetText = nil;
+            
+            tweetText = NSLocalizedString(@"@hpsupport #hpsprocket [Emter Text]", nil);
+            
+            if (IS_OS_8_OR_LATER) {
+                NSInteger numberOfPairedSprockets = [[MP sharedInstance] numberOfPairedSprockets];
+                
+                if (numberOfPairedSprockets > 0) {
+                    tweetText = [NSString stringWithFormat:@"@hpsupport #hpsprocket S: %@ P: %@ [Emter Text]", @(1), @(2)];
+                }
+            }
+            
+            [self.twitterComposeViewController setInitialText:tweetText];
+            
             [self presentViewController:self.twitterComposeViewController animated:YES completion:^{
                 
             }];
         }
+    }
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    UINavigationController *navigationController = (UINavigationController *) segue.destinationViewController;
+    PGWebViewerViewController *webViewerViewController = (PGWebViewerViewController *)navigationController.topViewController;
+    
+    if ([segue.identifier isEqualToString:@"ViewUserGuideSegue"]) {
+        webViewerViewController.trackableScreenName = @"View User Guide";
+        webViewerViewController.url = kPGHelpAndHowToViewUserURL;
+    } else if ([segue.identifier isEqualToString:@"VisitWebsiteSegue"]) {
+        webViewerViewController.trackableScreenName = @"Visit Website";
+        webViewerViewController.url = kPGHelpAndHowToVisitWebsiteURL;
+    } else if ([segue.identifier isEqualToString:@"JoinForumSupportSegue"]) {
+        webViewerViewController.trackableScreenName = @"Join Forum Support";
+        webViewerViewController.url = kPGHelpAndHowToJoinForumSupportURL;
     }
 }
 
