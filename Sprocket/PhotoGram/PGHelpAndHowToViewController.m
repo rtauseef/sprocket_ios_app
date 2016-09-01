9ce417546116bd6d8a58afe478e4782578c3c80e
Ivan Gialorenço <ivan.gialorenco@arctouch.com>
2016-08-31 18:21:45 -0700

    Adding Social framework

--- Sprocket/PhotoGram/PGHelpAndHowToViewController.m
+++ Sprocket/PhotoGram/PGHelpAndHowToViewController.m
@@ -13,8 +13,12 @@
 #import "PGHelpAndHowToViewController.h"
 #import "UIViewController+Trackable.h"
 
+#import <Social/Social.h>
+
 @interface PGHelpAndHowToViewController ()
 
+@property (strong, nonatomic) IBOutlet UITableViewCell *tweetSupportCell;
+@property (strong, nonatomic) SLComposeViewController *twitterComposeViewController;
 
 @end
 
@@ -25,6 +29,27 @@
     [super viewDidLoad];
     
     self.trackableScreenName = @"Help and How To Screen";
+    
+    self.twitterComposeViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+    [self.twitterComposeViewController setInitialText:@"First post from my iPhone app"];
+    [self.twitterComposeViewController addURL:[NSURL URLWithString:@"http://www.appcoda.com"]];
+    [self.twitterComposeViewController addImage:[UIImage imageNamed:@"test.jpg"]];
+    
+    __weak typeof(self) weakSelf = self;
+    [self.twitterComposeViewController setCompletionHandler:^(SLComposeViewControllerResult result) {
+        [weakSelf.tableView deselectRowAtIndexPath:[NSIndexPath indexPathForItem:1 inSection:1] animated:YES];
+    }];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 1) {
+        if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
+            [self presentViewController:self.twitterComposeViewController animated:YES completion:^{
+                
+            }];
+        }
+    }
 }
 
 @end
