f95a9d91564f97a742023167c7be5dab7e547e7d
Chrissy Harris <chrissy@hp.com>
2016-10-27 11:53:24 -0700

    [DE4139] Handling printer disconnect during printing/upgrading

--- Pods/MobilePrintSDK/Pod/Classes/Private/MPBTSessionController.m
+++ Pods/MobilePrintSDK/Pod/Classes/Private/MPBTSessionController.m
@@ -17,6 +17,8 @@ const char SPROCKET_SESSION_PACKET_LENGTH = 34;
 
 NSString *MPBTSessionDataReceivedNotification = @"MPBTSessionDataReceivedNotification";
 NSString *MPBTSessionDataSentNotification = @"MPBTSessionDataSentNotification";
+NSString *MPBTSessionAccessoryDisconnectedNotification = @"MPBTSessionAccessoryDisconnectedNotification";
+NSString *MPBTSessionStreamErrorNotification = @"MPBTSessionStreamErrorNotification";
 NSString *MPBTSessionDataBytesWritten = @"MPBTSessionDataBytesWritten";
 NSString *MPBTSessionDataTotalBytesWritten = @"MPBTSessionDataTotalBytesWritten";
 
@@ -225,7 +227,7 @@ static long long totalBytesWritten = 0;
 #pragma mark EAAccessoryDelegate
 - (void)accessoryDidDisconnect:(EAAccessory *)accessory
 {
-    // do something ...
+    [[NSNotificationCenter defaultCenter] postNotificationName:MPBTSessionAccessoryDisconnectedNotification object:self userInfo:nil];
 }
 
 #pragma mark NSStreamDelegateEventExtensions
@@ -249,6 +251,8 @@ static long long totalBytesWritten = 0;
             [self _writeData];
             break;
         case NSStreamEventErrorOccurred:
+            MPLogError(@"Accessory disconnected");
+            [[NSNotificationCenter defaultCenter] postNotificationName:MPBTSessionStreamErrorNotification object:self userInfo:nil];
             NSLog(@"StreamEvent: NSStreamEventErrorOccurred");
             break;
         case NSStreamEventEndEncountered:
