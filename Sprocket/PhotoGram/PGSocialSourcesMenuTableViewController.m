e36882473198c1e2a1d271542e21b537d5024f46
Ivan Gialorenço <ivan.gialorenco@arctouch.com>
2016-11-15 11:23:19 -0800

    Adding new files

---
+++ Sprocket/PhotoGram/PGSocialSourcesMenuTableViewController.m
@@ -0,0 +1,42 @@
+//
+// Hewlett-Packard Company
+// All rights reserved.
+//
+// This file, its contents, concepts, methods, behavior, and operation
+// (collectively the "Software") are protected by trade secret, patent,
+// and copyright laws. The use of the Software is governed by a license
+// agreement. Disclosure of the Software to third parties, in any form,
+// in whole or in part, is expressly prohibited except as authorized by
+// the license agreement.
+//
+
+#import "PGSocialSourcesMenuTableViewController.h"
+
+@interface PGSocialSourcesMenuTableViewController ()
+
+@end
+
+@implementation PGSocialSourcesMenuTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
+
