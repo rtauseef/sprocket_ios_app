43f98628efa10c162d8df995585bf403f010b5aa
Ivan Gialorenço <ivan.gialorenco@arctouch.com>
2016-12-14 15:39:53 -0800

    New Sprocket Landing Page

--- Sprocket/PhotoGram/PGSprocketLandingPageViewController.m
+++ Sprocket/PhotoGram/PGSprocketLandingPageViewController.m
@@ -10,9 +10,19 @@
 // the license agreement.
 //
 
-#import "PGSprocketLandingPageViewController.h"
+#import <MP.h>
 
-@interface PGSprocketLandingPageViewController ()
+#import "PGSprocketLandingPageViewController.h"
+#import "PGAppAppearance.h"
+#import "PGRevealViewController.h"
+#import "UIFont+Style.h"
+#import "UIColor+Style.h"
+#import "UIViewController+Trackable.h"
+
+@interface PGSprocketLandingPageViewController () <UITableViewDataSource, UITableViewDelegate, MPSprocketDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSString *connectedSprocketName;
 
 @end
 
@@ -21,7 +31,111 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.trackableScreenName = @"Sprocket Landing Screen";
     
+    [PGAppAppearance addGradientBackgroundToView:self.view];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(deselectRowTableViewCell)
+                                                 name:UIApplicationDidBecomeActiveNotification
+                                               object:nil];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self deselectRowTableViewCell];
+    
+    if ([[MP sharedInstance] numberOfPairedSprockets] == 1) {
+        [[MP sharedInstance] checkSprocketForUpdates:self];
+    } else {
+        self.connectedSprocketName = nil;
+        [self.tableView reloadData];
+    }
+}
+
+- (void)deselectRowTableViewCell
+{
+    [self.tableView deselectRowAtIndexPath:[self.tableView indexPathForSelectedRow] animated:YES];
+}
+
+- (IBAction)doneButtonTapped:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)openSettings
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+}
+
+- (void)didReceiveSprocketName:(NSString *)name
+{
+    self.connectedSprocketName = name;
+    [self.tableView reloadData];
+}
+
+#pragma mark TableView Datasource Methods
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"sprocketCellReuse"];
+    cell.textLabel.textColor = [UIColor whiteColor];
+    cell.detailTextLabel.textColor = [UIColor whiteColor];
+    cell.backgroundColor = [UIColor HPGrayColor];
+    
+    UIView *selectionColorView = [[UIView alloc] init];
+    selectionColorView.backgroundColor = [UIColor HPTableRowSelectionColor];
+    cell.selectedBackgroundView = selectionColorView;
+    
+    switch (indexPath.row) {
+        case 0: {
+            cell.textLabel.text = NSLocalizedString(@"App Settings", nil);
+            cell.detailTextLabel.hidden = YES;
+            break;
+        }
+        case 1: {
+            cell.textLabel.text = NSLocalizedString(@"Printers", nil);
+            cell.detailTextLabel.hidden = NO;
+            cell.detailTextLabel.text = self.connectedSprocketName;
+            break;
+        }
+        default:
+            break;
+    }
+    
+    [cell layoutIfNeeded];
+    
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 2;
+}
+
+#pragma mark TableView Delegate Methods
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    switch (indexPath.row) {
+        case 0: {
+            [self openSettings];
+            break;
+        }
+        case 1: {
+            [[MP sharedInstance] presentBluetoothDevicesFromController:self animated:YES completion:nil];
+            break;
+        }
+        default:
+            break;
+    }
 }
 
 @end
