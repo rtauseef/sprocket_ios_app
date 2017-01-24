ec61760d545b53fa426e44403228ccc3d95353d9
Ivan Gialorenço <ivan.gialorenco@arctouch.com>
2017-01-23 17:30:38 -0800

    adding sprocket infoPlist.strings file to localization script

--- localization_strings.sh
+++ localization_strings.sh
@@ -3,8 +3,11 @@
 # Generate Xliff files of Sprocket App
 xcodebuild -exportLocalizations -localizationPath ./localizations/sprocket/ -project Sprocket/Sprocket.xcodeproj -exportLanguage en
 
-#generate Localizable.strings of ios-print-sdk
+# Copying strings file needed
+cp Sprocket/PhotoGram/en.lproj/InfoPlist.strings localizations/sprocket/
+
+# Generate Localizable.strings of ios-print-sdk
 genstrings -o localizations/ios-print-sdk/ -s MPLocalizedString Pods/MobilePrintSDK/Pod/Classes/Public/*.m Pods/MobilePrintSDK/Pod/Classes/Private/*.m
 
-#generate Localizable.strings of ios-print-sdk
+# Generate Localizable.strings of ios-print-sdk
 genstrings -o localizations/hp-photo-provider/ -s HPPRLocalizedString Pods/HPPhotoProvider/Pod/Classes/*.m
