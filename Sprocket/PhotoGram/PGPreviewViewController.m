d00a7c0b0418cbab6391e0e9add07068b30cc470
Peter <pnguyen@hp.com>
2017-06-09 18:30:58 -0700

    DE5534 set the editedImage to be the Screenshot when printing

--- Sprocket/PhotoGram/PGPreviewViewController.m
+++ Sprocket/PhotoGram/PGPreviewViewController.m
@@ -929,6 +929,7 @@ static CGFloat kAspectRatio2by3 = 0.66666666667;
                              }
                          }];
         } else {
+            gestureView.editedImage = [gestureView screenshotImage];
             block(gestureView.editedImage);
         }
     }
