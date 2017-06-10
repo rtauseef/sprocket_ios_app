ae66de91c6caa4230691812a2993c4b77866825b
Chrissy Harris <chrissy@hp.com>
2017-06-09 17:15:50 -0700

    Discard and adjust work... work in progress, saving for later

--- Sprocket/PhotoGram/PGImglyManager.m
+++ Sprocket/PhotoGram/PGImglyManager.m
@@ -18,6 +18,7 @@
 #import <imglyKit/IMGLYAnalyticsConstants.h>
 
 static NSString * const kImglyMenuItemMagic = @"Magic";
+static NSString * const kImglyMenuItemAdjust = @"Adjust";
 static NSString * const kImglyMenuItemFilter = @"Filter";
 static NSString * const kImglyMenuItemFrame = @"Frame";
 static NSString * const kImglyMenuItemSticker = @"Sticker";
@@ -70,6 +71,10 @@ int const kCustomButtonTag = 9999;
                                                                        }
                                                                         state:nil];
 
+    IMGLYBoxedMenuItem *adjustItem = [[IMGLYBoxedMenuItem alloc] initWithTitle:kImglyMenuItemAdjust
+                                                                          icon:[UIImage imageNamed:@"ic_adjust_48pt"]
+                                                                          tool:[[IMGLYAdjustToolController alloc] initWithConfiguration:configuration]];
+
     IMGLYBoxedMenuItem *filterItem = [[IMGLYBoxedMenuItem alloc] initWithTitle:kImglyMenuItemFilter
                                                                           icon:[UIImage imageNamed:@"ic_filter_48pt"]
                                                                           tool:[[IMGLYFilterToolController alloc] initWithConfiguration:configuration]];
@@ -92,6 +97,7 @@ int const kCustomButtonTag = 9999;
 
     return @[
              magicItem,
+             adjustItem,
              filterItem,
              frameItem,
              stickerItem,
@@ -228,6 +234,11 @@ int const kCustomButtonTag = 9999;
             photoEditorBuilder.titleViewConfigurationClosure = [self titleBlockWithAccessibilityLabel:@"editor-tool-screen"];
 
             photoEditorBuilder.applyButtonConfigurationClosure = [self applyButtonBlockWithAccessibilityLabel:@"editor-tool-apply-btn"];
+            
+            photoEditorBuilder.discardConfirmationClosure = ^(IMGLYPhotoEditViewController * _Nonnull photoEditViewController, void (^ _Nonnull closure)(void)) {
+                [embellishmentMetricsManager clearAllEmbellishmentMetrics];
+                [photoEditViewController dismissViewControllerAnimated:YES completion:nil];
+            };
 
             PGEmbellishmentMetric *autofixMetric = [[PGEmbellishmentMetric alloc] initWithName:@"Auto-fix" andCategoryType:PGEmbellishmentCategoryTypeEdit];
 
@@ -247,6 +258,9 @@ int const kCustomButtonTag = 9999;
                         cell.imageView.highlighted = YES;
                     }
 
+                } else if ([item.title isEqualToString:kImglyMenuItemAdjust]) {
+                    cell.accessibilityIdentifier = @"editAdjust";
+                    
                 } else if ([item.title isEqualToString:kImglyMenuItemFilter]) {
                     cell.accessibilityIdentifier = @"editFilters";
 
@@ -276,6 +290,22 @@ int const kCustomButtonTag = 9999;
 
         }];
 
+        // Adjust configuration
+        
+        [builder configureAdjustToolController:^(IMGLYAdjustToolControllerOptionsBuilder * _Nonnull toolBuilder) {
+            toolBuilder.allowedAdjustToolsAsNSNumbers = @[[NSNumber numberWithInteger:AdjustToolBrightness],
+                                                          [NSNumber numberWithInteger:AdjustToolContrast],
+                                                          [NSNumber numberWithInteger:AdjustToolSaturation]];
+
+            toolBuilder.sliderConfigurationClosure = ^(IMGLYSlider * _Nonnull slider) {
+                slider.filledTrackColor = [UIColor HPBlueColor];
+                slider.thumbTintColor = [UIColor HPBlueColor];
+            };
+            
+            toolBuilder.adjustToolButtonConfigurationClosure = ^(IMGLYIconCaptionCollectionViewCell * _Nonnull cell, enum AdjustTool tool) {
+                cell.captionLabel.text = nil;
+            };
+        }];
 
         // Filters configuration
 
@@ -369,7 +399,6 @@ int const kCustomButtonTag = 9999;
                 PGEmbellishmentMetric *stickerMetric = [[PGEmbellishmentMetric alloc] initWithName:sticker.accessibilityLabel andCategoryType:PGEmbellishmentCategoryTypeSticker];
                 [embellishmentMetricsManager addEmbellishmentMetric:stickerMetric];
             };
-
         }];
 
         [builder configureStickerColorToolController:^(IMGLYStickerColorToolControllerOptionsBuilder * _Nonnull toolBuilder) {
